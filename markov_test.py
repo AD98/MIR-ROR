6cdf35e8b43fad5bc3d218947d5fe5c1c2167a02
Anshul Doshi <anshuldoshi@Anshuls-MacBook-Pro.local>
2018-07-02 13:46:59 -0400

    added rhythm generation

--- markov_test.py
+++ markov_test.py
@@ -17,36 +17,64 @@ def to_stream(song):
   ret = stream.Stream()
   for i in range(len(song)):
     n = note.Note()
-    n.pitch.midi = song[i]
-    n.quarterLength = my_notes[i % len(my_notes)].quarterLength
+    n.pitch.midi = song[i] % 128
+    ql = int(song[i] / 128);
+    n.quarterLength = get_dur(ql)
+    #n.quarterLength = my_notes[i % len(my_notes)].quarterLength
     ret.append(n)
   return ret
+
+def get_ql(n):
+  if (n.quarterLength >= 4):
+    return 0
+  elif (n.quarterLength >= 2):
+    return 1
+  elif (n.quarterLength >= 1):
+    return 2
+  elif (n.quarterLength >= 0.5):
+    return 3
+  return 4
+
+def get_dur(num):
+  if (num == 0):
+    return 4
+  elif (num == 1):
+    return 2
+  elif (num == 2):
+    return 1
+  elif (num == 3):
+    return 0.5
+  return 0.25
+
 
 # load in a random monophonic piece from corpus
 bundle = corpus.search(1, 'numberOfParts')#[0].parse()
 
-mat1 = np.zeros((128,128))
-mat2 = np.zeros((128,128,128))
+mat1 = np.zeros((128*5,128*5))
+mat2 = np.zeros((128*5,128*5,128*5))
 my_notes = None
+
 for j in range(8):
   orig = bundle[j].parse()
   print("Title: ",orig.metadata.title)
 
-# flatten stream
+  # flatten stream
   my_notes = orig.flat.notes
 
-# initialize and fill in frequency matrix
-
-  mat1[my_notes[0].pitch.midi][my_notes[1].pitch.midi] += 1
-  for i in range (2, len(my_notes)):
-    mat1[my_notes[i-1].pitch.midi][my_notes[i].pitch.midi] += 1
-    mat2[my_notes[i-2].pitch.midi][my_notes[i-1].pitch.midi][my_notes[i].pitch.midi] += 1
+  # initialize and fill in frequency matrix
+  for i in range (1, len(my_notes)):
+    ql_0 = get_ql(my_notes[i])
+    ql_1 = get_ql(my_notes[i-1])
+    mat1[128*ql_1 + my_notes[i-1].pitch.midi][128*ql_0 + my_notes[i].pitch.midi] += 1
+    if (i > 1):
+      ql_2 = get_ql(my_notes[i-2])
+      mat2[128*ql_2 + my_notes[i-2].pitch.midi][128*ql_1 + my_notes[i-1].pitch.midi][128*ql_0 + my_notes[i].pitch.midi] += 1
 
 #print(mat)
 
-# set first note in generated song to first note in original song
-song1 = [my_notes[0].pitch.midi] 
-song2 = [my_notes[0].pitch.midi, my_notes[1].pitch.midi]
+# set first note(s) in generated song to first note in original song
+song1 = [128*get_ql(my_notes[0]) + my_notes[0].pitch.midi] 
+song2 = [128*get_ql(my_notes[0]) + my_notes[0].pitch.midi, 128*get_ql(my_notes[1]) + my_notes[1].pitch.midi]
 
 # generate song
 for i in range (len(my_notes)):
