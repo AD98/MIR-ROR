13ee6a02345d00459ee9ffea3aaaefe822916454
Anshul Doshi <anshuldoshi@Anshuls-MBP.home>
2018-09-06 15:50:43 -0400

    change READ.ME to README.txt

--- ignore/sampling.py
+++ ignore/sampling.py
@@ -1,13 +1,34 @@
 from scipy.io import wavfile
 import numpy as np
-from shared import *
 
+def get_context(my_song, max_motif):
+  if (len(my_song) < max_motif):
+    return my_song
+  else:
+    return my_song[len(my_song) - max_motif:]
 
+def get_note(rand_num, next_notes):
+  sum_freq = 0.0
+  for j in range(len(next_notes)):
+    sum_freq += next_notes[j]
+  if (sum_freq == 0):
+    return None
+  num = 0.0
+  for j in range(len(next_notes)):
+    frac = (next_notes[j]/sum_freq)*100
+    num += frac
+    if (num > rand_num):
+      return j
+
+def get_error(list1, list2):
+  ret = 0
+  for i in range(len(list1)):
+    ret += abs(list1[i] - list2[i])
+  return ret
 
 def find_key(keys, motif, allowed_error):
-  print('len(motif)',len(motif))
-  for key in keys:
-    print('len(key)',len(key))
+  found = False
+  for key in m_dict.keys():
     if (len(key) != len(motif)):
       continue
     if (get_error(list(key), motif) < allowed_error):
@@ -20,13 +41,17 @@ def get_channel(to_flatten, channel):
     ret.append(to_flatten[i][channel])
   return ret
 
-fs, data = wavfile.read('/Users/anshuldoshi/WINLAB/mir-ror/drake_nfw.wav')
+def get_next_notes(l):
+  ret = []
+  for elem in l:
+    ret.append(elem[1])
+  return ret
+
+fs, data = wavfile.read('/Users/anshuldoshi/Downloads/drake_nfw.wav')
 data = get_channel(data.tolist(), 0)
 
-data = data[:1000]
-
-sample_len = 20#fs * 3
-allowed_error = 65536 * 0.80 * sample_len
+sample_len = fs * 3
+allowed_error = 65536 * 0.05 * sample_len
 
 m_dict = {}
 motif = []
@@ -75,28 +100,20 @@ cur_samp = 0
 context = []
 my_song = []
 
-#print(c_dict)
-
 for i in range(50):
-  #print('my_song',my_song)
-  #print('context',context)
-  key = find_key(c_dict.keys(), context, allowed_error)
   if (len(context) == 0):
     my_song += data[sample_len*cur_samp : sample_len*(cur_samp + 1)]
     context = get_context(my_song, max_motif)
     cur_samp += 1
     print('no motif')
-  elif ((key is not None)): #and (len(key) < max_motif)):#in c_dict):
+  elif (find_key(c_dict.keys(), context, allowed_error) != None):
     rand_num = np.random.randint(0,100)
-    #print('key',key)
+    key = find_key(c_dict.keys(), context, allowed_error)
     next_notes = get_next_notes(c_dict[key])
     to_app = c_dict[key][get_note(rand_num, next_notes)][0]
-    my_song += to_app
+    my_song.append(to_app)
     context = get_context(my_song, max_motif)
-    print('match')
   else:
     context = context[sample_len:]
 
-arr = np.array(my_song, dtype='int32')
-print(arr)
-wavfile.write('wave_lz.wav', fs, arr)
+wavfile.write('wave_lz.wav', fs, np.array(my_song))
